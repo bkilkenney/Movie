97ea58da9a7e8ce1c56a49dadd364cc7dd33f7c6
Brad Kilkenney <bkilkenney@gmail.com>
2016-10-02 23:31:02 -0400

    Initial Commit

---
+++ Controllers/MoviesController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller   //Added this controller which provides methods(actions)
+                                                //MoviesController derives from Controller class(Inherited)
+    {
+        // GET: Movies
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Shrek!" };        //Creating object from the Movie class
+                                                                //This will usually come from a DB but were just building from scratch
+            return View(movie);  //(movie) is the model
+        }
+    }
+}
---
+++ Models/Movie.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Vidly.Models
+{
+    public class Movie
+    {
+
+        //Properties used to represent state
+
+        public int Id {
+            get;
+            set;
+        }
+             
+        public string Name {
+            get;
+            set;
+        }
+
+        // /movies/random - Creating a page that randomly selects movies and displays its properties, need a CONTROLLER
+
+
+
+
+        
+    }
+}
