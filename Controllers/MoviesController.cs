5e190d4cb8c889d2be42e47fc47fa7f7243fb3cf
Brad Kilkenney <bkilkenney@yahoo.com>
2016-11-07 16:04:51 -0500

    staged files

--- Controllers/MoviesController.cs
+++ Controllers/MoviesController.cs
@@ -10,12 +10,43 @@ namespace Vidly.Controllers
     public class MoviesController : Controller   //Added this controller which provides methods(actions)
                                                 //MoviesController derives from Controller class(Inherited)
     {
-        // GET: Movies
+        // GET: Movies/Random  = Controller/Action
         public ActionResult Random()
         {
-            var movie = new Movie() { Name = "Shrek!" };        //Creating object from the Movie class
-                                                                //This will usually come from a DB but were just building from scratch
-            return View(movie);  //(movie) is the model
+            var movie = new Movie() { Name = "Shrek!" };  //This will usually come from a DB but were just building from scratch
+
+            return View();
+
+
+
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return Content("id=" + id);
+        }
+
+
+       
+
+           
+            
+            
+            
+            
+            //Action results below
+            
+            /*return RedirectToAction("Index", "Home", new { page = 1, sortBy = "name" });*/  //("Name of the action", "Controller")
+
+
+            /*return new EmptyResult();*/  
+            //return View();
+            //return Content("Hello World");  //(movie) is the model
+            //return HttpNotFound();
+
+        //These are different results you can get back as your ActionResult
+
+
+
         }
     }
-}
